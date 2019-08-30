fbc786ad19be5d22c4672d1887dbb50eaad845ad
Moises Cavalcanti <moisesc.bezerril@gmail.com>
2019-08-30 00:12:03 -0300

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <LoRaMesh.h>
+// Variáveis a serem preenchidas com o recebido da mensagem
+uint16_t id;
+uint8_t payload[31], payloadSize;
 
 /* Local device ID */
 uint16_t localId;
@@ -24,41 +27,55 @@ uint16_t AdcIn[2];
 
 /* SoftwareSerial handles */
 SoftwareSerial* hSerialCommands = NULL;
+SoftwareSerial* hSerialTransparent = NULL;
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //Configurando o módulo LoRaMesh
-  delay(1000);
-  Serial.begin(9600); /* Initialize monitor serial */
-  
-  /* Initialize SoftwareSerial */
-  //Pinos:
-  // 7-> Lora TX
-  // 6-> Lora RX
-  hSerialCommands = SerialCommandsInit(7, 6, 9600);
-  
-  /* Gets the local device ID */
-  if(LocalRead(&localId, &localNet, &localUniqueId) != MESH_OK)
-    Serial.print("Couldn't read local ID\n\n");
-  else
-  {
-    Serial.print("Local ID: ");
-    Serial.println(localId);
-    Serial.print("Local NET: ");
-    Serial.println(localNet);
-    Serial.print("Local Unique ID: ");
-    Serial.println(localUniqueId, HEX);
-    Serial.print("\n");
-  }
-  
-  delay(500);
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN,
+                 HIGH);  // turn the LED on (HIGH is the voltage level)
+    // Configurando o módulo LoRaMesh
+    delay(1000);
+    Serial.begin(9600); /* Initialize monitor serial */
+
+    /* Initialize SoftwareSerial */
+    // Pinos:
+    // 7-> Lora TX
+    // 6-> Lora RX
+    hSerialCommands = SerialCommandsInit(7, 6, 9600);
+
+    // 4-> Lora TX
+    // 5-> Lora RX
+    hSerialTransparent = SerialTranspInit(4, 5, 9600);
+
+    /* Gets the local device ID*/
+    if(LocalRead(&localId, &localNet, &localUniqueId) != MESH_OK)
+      Serial.print("Couldn't read local ID\n\n");
+    else
+    {
+      Serial.print("Local ID: ");
+      Serial.println(localId);
+      Serial.print("Local NET: ");
+      Serial.println(localNet);
+      Serial.print("Local Unique ID: ");
+      Serial.println(localUniqueId, HEX);
+      Serial.print("\n");
+    }
+
+    delay(500);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+    if (ReceivePacketTransp(&id, &payload[0], &payloadSize, 10000) == MESH_OK) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("RECEBIDO:");
+        Serial.print("tamanho: ");
+        Serial.println(payloadSize);
+        for (int i = 0; i < payloadSize; i++) Serial.println(payload[i], HEX);
+    } else {
+        Serial.println("ERRO AO RECEBER PACOTE");
+    }
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
